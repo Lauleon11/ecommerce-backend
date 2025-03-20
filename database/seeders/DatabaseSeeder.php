3bf3a04e87fe628acb73690b64dcd0f4fb407af8
Lauleon11 <lleon598@unab.edu.co>
2025-03-20 12:34:36 -0500

    creacion factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,5 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class
         ]);
+
+        Category::factory(100)->create();
+
     }
 }
